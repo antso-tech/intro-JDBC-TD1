2f4538af1c481613137b0f57c2897a2443d8640e
antso.tech <hei.antso.2007@gmail.com>
2025-12-05 16:05:00 +0300

    insertion des données dans data.sql

--- data.sql
+++ data.sql
@@ -3,3 +3,19 @@ INSERT INTO Product (name, price) VALUES ('IPhone 13', 5200.00);
 INSERT INTO Product (name, price) VALUES ('Casque Sony WH1000', 890.50);
 INSERT INTO Product (name, price) VALUES ('Clavier Logitech', 180.00);
 INSERT INTO Product (name, price) VALUES ('Ecran Samsung 27', 1200.00);
+
+INSERT INTO Product_category (name, id_product) VALUES ('Informatique', 1)
+
+INSERT INTO Product_category (name, id_product) VALUES ('Téléphone',2)
+
+INSERT INTO Product_category (name, id_product) VALUES ('Audio',3)
+
+INSERT INTO Product_category (name, id_product) VALUES ('Accessoires',4)
+
+INSERT INTO Product_category (name, id_product) VALUES ('Informatique',5)
+
+INSERT INTO Product_category (name, id_product) VALUES ('Bureau',5)
+
+INSERT INTO Product_category (name, id_product) VALUES ('Mobile',2)
+
+
