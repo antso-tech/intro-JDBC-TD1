8be9c65205ba37d811f2373cce09e10d4f100509
antso.tech <hei.antso.2007@gmail.com>
2025-12-05 15:54:18 +0300

    ajout des bases de données produits

---
+++ data.sql
@@ -0,0 +1,5 @@
+INSERT INTO Product (name, price) VALUES ('Laptop Dell XPS', 4500.00);
+INSERT INTO Product (name, price) VALUES ('IPhone 13', 5200.00);
+INSERT INTO Product (name, price) VALUES ('Casque Sony WH1000', 890.50);
+INSERT INTO Product (name, price) VALUES ('Clavier Logitech', 180.00);
+INSERT INTO Product (name, price) VALUES ('Ecran Samsung 27', 1200.00);
