2e9885fc124903eb44401d117ff2f3e3f934ad11
antso.tech <hei.antso.2007@gmail.com>
2025-12-05 09:14:02 +0300

    initiation des bases des données avec users

---
+++ init_db.sql
@@ -0,0 +1,10 @@
+--1--
+CREATE DATABASE product_management_db;
+CREATE USER product_manager_user WITH PASSWORD '123456'
+
+GRANT CONNECT ON DATABASE product_management_db TO product_manager_user
+
+GRANT CREATE ON SCHEMA PUBLIC TO product_manager_user 
+
+
+
