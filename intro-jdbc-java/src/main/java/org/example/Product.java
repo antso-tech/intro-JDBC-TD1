edfff003d8bbb8a4dfe97b8ca83db70a864315af
antso.tech <hei.antso.2007@gmail.com>
2025-12-05 16:17:18 +0300

    ajout des attributs et de getCategory

--- intro-jdbc-java/src/main/java/org/example/Product.java
+++ intro-jdbc-java/src/main/java/org/example/Product.java
@@ -1,4 +1,14 @@
 package org.example;
 
+import java.time.Instant;
+
 public class Product {
+    private int id;
+    private String name;
+    private Instant creationDateTime;
+    private Category category;
+
+    public Category getCategory() {
+        return category;
+    }
 }
